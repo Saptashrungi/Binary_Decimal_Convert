996946ef8245d8963dd3411609bc95d077d5ef75
Saptashrungi <saptashrungib@gmail.com>
2019-07-07 20:20:15 +0530

    Project commit

---
+++ bintodec.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int decnum;
+    cin>>decnum;
+    int power=0;
+    int sum=0;
+    while(decnum)
+    {
+      sum+=(decnum%10)*pow(2,power);
+      decnum=decnum/10;
+      ++power;
+    };
+    cout<<sum;
+    return 0;
+}
